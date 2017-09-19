0b82f535fcb7ed13032781c2b7fdf7bd644c76a2
Arvind Pinto <arvindpinto90@yahoo.com>
2017-09-19 21:06:38 +0530

    - Added a function to process repeats in a measure. Repeats are basically associated with <barline> XML elements
    - Added a placeholder function to process notes

--- bin/MusicXMLConverter.py
+++ bin/MusicXMLConverter.py
@@ -6,6 +6,7 @@ class MusicXMLConverter:
         pass
 
     def measureIsEmpty(self, inMeasureElem):
+        """Check if a measure is empty. A measure is empty if all the notes in it are rest"""
         inMeasureNotes = inMeasureElem.findall("note")
         if (len(inMeasureNotes) > 0):
             restCount = 0;  # initialise rest count
@@ -19,9 +20,65 @@ class MusicXMLConverter:
             else:
                 return False
 
+    def processNotes(self, inMeasureElem):
+        "Process notes in a element"
+        pass
+
+    def processRepeatsInMeasure(self, inMeasureElem):
+        """Handle repeats. Like barlines, voltas etc. Returns a list of "barline" XML elements"""
+        inBarlines = inMeasureElem.findall("barline")
+        # list to hold array of barlines for the repeats
+        outBarlines = []
+
+        if (len(inBarlines) > 0):
+            for inBarline in inBarlines:
+                outBarline = ET.Element("barline")
+                outBarlineEnding = ET.Element("ending")
+                outBarlineRepeat = ET.Element("repeat")
+
+                inBarlineAttribs = inBarline.attrib
+                for inBarlineAttrib in inBarlineAttribs:
+                    outBarline.set(inBarlineAttrib, inBarlineAttribs[inBarlineAttrib])
+
+                # important sub elements we care about are <repeat> and <ending> others are not important
+                inBarlineEnding = inBarline.find("ending")
+                if (inBarlineEnding is not None):
+                    inBarlineEndingAttribs = inBarlineEnding.attrib
+                    for inBarlineEndingAttrib in inBarlineEndingAttribs:
+                        outBarlineEnding.set(inBarlineEndingAttrib, inBarlineEndingAttribs[inBarlineEndingAttrib])
+
+                    #append barline ending to the outBarline
+                        outBarline.append(outBarlineEnding)
+
+                inBarlineRepeat = inBarline.find("repeat")
+                if (inBarlineRepeat is not None):
+                    inBarlineRepeatAttribs = inBarlineRepeat.attrib
+
+                    # add all normal repeat attribs
+                    for inBarlineRepeatAttrib in inBarlineRepeatAttribs:
+                        outBarlineRepeat.set(inBarlineRepeatAttrib, inBarlineRepeatAttribs[inBarlineRepeatAttrib])
+
+                    #Special PS case -- PS needs the times attrib irrespective even for 1 repeat time
+                    if ("backward" == inBarlineRepeat.get("direction")):
+                        if ("times" in inBarlineRepeatAttribs):
+                            outBarlineRepeat.set("times",inBarlineAttribs["times"])
+                        else:
+                            outBarlineRepeat.set("times", "1")
+
+                    #append barline ending to the outBarline
+                    outBarline.append(outBarlineRepeat)
+                else:
+                    print("repeat elem not found inside barline")
+
+                # finally add the barlines to the list
+                outBarlines.append(outBarline)
+
+        return outBarlines
+
     def processFirstMeasure(self, inMeasureElem):
+        """Does special handling for the first measure as it includes important information like time signature etc."""
         if (self.measureIsEmpty(inMeasureElem)):
-            print("Fatal Error: The first measure caannot be empty in your piece")
+            print("Fatal Error: The first measure cannot be empty in your piece")
             exit()
 
         outMeasure = ET.Element("measure")
@@ -29,10 +86,20 @@ class MusicXMLConverter:
         outMeasure.set("number", str(inMeasureNo))
 
         if (inMeasureNo == 1):
+            # root
             outMeasureAttributes = ET.Element("attributes")
+
+            # divisions
             outMeasureDivisions = ET.Element("divisions")
+
+            # key
             outMeasureKey = ET.Element("key")
             outMeasureFifths = ET.Element("fifths")
+
+            # time
+            outMeasureTime = ET.Element("time")
+            outMeasureBeats = ET.Element("beats")
+            outMeasureBeatType = ET.Element("beat-type")
 
             # process attributes root
             inMeasureAttributes = inMeasureElem.find("attributes")
@@ -64,10 +131,47 @@ class MusicXMLConverter:
                 print("Fatal Error: input measure key not found. Exiting")
                 exit()
 
+            # process time
+            inMeasureTime = inMeasureAttributes.find("time")
+            if (inMeasureTime is None):
+                print("Fatal Error: time not found in measure. Exiting")
+                exit()
+
+            inMeasureBeats = inMeasureTime.find("beats")
+
+            if (inMeasureBeats is not None):
+                outMeasureBeats.text = inMeasureBeats.text
+            else:
+                print("Fatal Error: beats not found in measure. Exiting")
+                exit()
+
+            inMeasureBeatType = inMeasureTime.find("beat-type")
+            if (inMeasureBeatType is not None):
+                outMeasureBeatType.text = inMeasureBeatType.text
+            else:
+                print("Fatal Error: beats not found in measure. Exiting")
+                exit()
+
+            outMeasureTime.append(outMeasureBeats)
+            outMeasureTime.append(outMeasureBeatType)
+
             # creating the nesting hierarchy
+            # divisions
             outMeasureAttributes.append(outMeasureDivisions)
+            # key
             outMeasureAttributes.append(outMeasureKey)
+            # time
+            outMeasureAttributes.append(outMeasureTime)
+            # add any repeats
+            outBarlines = self.processRepeatsInMeasure(inMeasureElem)
+            if (len(outBarlines) > 0):
+                for outBarline in outBarlines:
+                    outMeasure.append(outBarline)
+            else:
+                print("No barlines found")
+            # add it all to the first measure
             outMeasure.append(outMeasureAttributes)
+
             return outMeasure
         else:
             print("Error while processing first measure got an unexpected measure number ", inMeasureNo, ". Expected  ",
@@ -80,7 +184,7 @@ class MusicXMLConverter:
 
         # parse input file and store the root
         inputTree = ET.parse(musescoreXMLfilePath)
-        inputTreeRoot = inputTree.getroot()
+        # inputTreeRoot = inputTree.getroot()
 
         # create the root output element
         outTreeRoot = ET.Element("score-partwise")
@@ -118,11 +222,12 @@ class MusicXMLConverter:
             for i in range(0, len(inMeasures)):
                 print("Processing measure", inMeasures[i].get("number"))
 
+                # First measure
                 # Do this irrespective of whether measure is empty or not
                 if (0 == i):
                     outPart.append(self.processFirstMeasure(inMeasures[i]))
-                    ''' important process it only with this function and
-                        skip the other code it otherwise do not execute below code '''
+                    ''' important process it only with the func above function and
+                        skip the other below code '''
                     continue
 
                 if (self.measureIsEmpty(inMeasures[i])):
